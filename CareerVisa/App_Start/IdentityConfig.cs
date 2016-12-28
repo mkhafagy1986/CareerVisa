a89b9755aff4ea0f89ce189634fa6885accb7b7c
mkhafagy1986 <eng.mohammedkhafagy@gmail.com>
2016-12-28 14:52:40 +0300

    CareerVisa_Password_Allow

--- CareerVisa/App_Start/IdentityConfig.cs
+++ CareerVisa/App_Start/IdentityConfig.cs
@@ -105,10 +105,10 @@ namespace CareerVisa
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
